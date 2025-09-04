7e76811e24570d1f62565409199f0b7641f8302e
Egor Danilov <danilov-egor-e@yandex.ru>
2025-09-04 10:03:01 +0000

    added yaml compair functional

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,10 +9,18 @@ const __dirname = dirname(__filename)
 const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
 const readFile = filename => fs.readFileSync(getFixturePath(filename), 'utf-8')
 
-test('flat-files compairison', () => {
+test('flat-files-json compairison', () => {
   const file1 = getFixturePath('file1.json')
   const file2 = getFixturePath('file2.json')
   const result = genDiff(file1, file2)
-  const expected = readFile('expected-jsonCompair.txt')
+  const expected = readFile('expected-Compair.txt')
+  expect(result.trim()).toEqual(expected.trim())
+})
+
+test('flat-files-yaml compairison', () => {
+  const file1 = getFixturePath('file1.yaml')
+  const file2 = getFixturePath('file2.yaml')
+  const result = genDiff(file1, file2)
+  const expected = readFile('expected-Compair.txt')
   expect(result.trim()).toEqual(expected.trim())
 })
